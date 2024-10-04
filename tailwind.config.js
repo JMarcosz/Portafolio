904ebc1dd2984170e9ae2092e2c20816ae58e082
Jean Marte <jeanmarte22@gmail.com>
2024-10-04 12:50:27 -0400

    Add Footer v0.1

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  purge: [
+
+    './src/**/*.html',
+
+    './src/**/*.js',
+
+  ],
+   darkMode: true, // or 'media' or 'class'
+   theme: {
+    colors: {
+      'primary': '#4B7CF9',
+      'secondary': '#373A3D',
+      's-white': '#F3F4F6',
+      'b-black': '#181717',
+      'g-gray': '#292C2F',
+    },
+   },
+   variants: {},
+   plugins: [],
+ }
